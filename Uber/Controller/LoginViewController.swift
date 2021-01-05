2dea037d981aa722d53e4826dadbbdc816474b33
Ben Williams <benryanwilliams@hotmail.com>
2021-01-06 02:45:43 +0700

    Initial commit

---
+++ Uber/Controller/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  Uber
+//
+//  Created by Ben Williams on 05/01/2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
